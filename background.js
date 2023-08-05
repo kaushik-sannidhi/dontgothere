1365eafdaa2ac56e9e4d01f851f7ed9782f9fdf5
kaushik-sannidhi <kaushik.sannidhi@gmail.com>
2023-08-05 18:40:08 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,18 +2,21 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
         const tabUrl = tab.url;
         const iconUrl = chrome.runtime.getURL('dontgothere16.png');
-        chrome.storage.sync.get(['baseURL', 'redirectURL'], function (result) {
-            const { baseURL, redirectURL } = result;
-            if (baseURL && redirectURL && tabUrl.includes(baseURL)) {
-                chrome.tabs.remove(tabId, function () {
-                    chrome.tabs.create({ url: redirectURL });
-                });
-                chrome.notifications.create({
-                    type: 'basic',
-                    iconUrl: iconUrl,
-                    title: 'Tab Updated',
-                    message: baseURL + " --- " + redirectURL,
-                });
+        chrome.storage.sync.get('rules', function (result) {
+            const rules = result.rules || [];
+            for (const rule of rules) {
+                if (rule.baseURL && rule.redirectURL && tabUrl.includes(rule.baseURL)) {
+                    chrome.tabs.remove(tabId, function () {
+                        chrome.tabs.create({ url: rule.redirectURL });
+                    });
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: iconUrl,
+                        title: 'Tab Updated',
+                        message: rule.baseURL + " --- " + rule.redirectURL,
+                    });
+                    break; 
+                }
             }
         });
     }
