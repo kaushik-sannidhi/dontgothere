7fd1d41883e192882e4930bf16bc55c5253bb8a0
kaushik-sannidhi <kaushik.sannidhi@gmail.com>
2023-08-05 18:39:23 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,67 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const saveButton = document.getElementById('saveButton');
+    const baseURLInput = document.getElementById('baseURL');
+    const redirectURLInput = document.getElementById('redirectURL');
+    const addRuleButton = document.getElementById('addRule');
+    const rulesList = document.getElementById('rulesList');
 
-    saveButton.addEventListener('click', function () {
-        const baseURL = document.getElementById('baseURL').value;
-        const redirectURL = document.getElementById('redirectURL').value;
-
-        if (baseURL && redirectURL) {
-            chrome.storage.sync.set({ baseURL, redirectURL }, function () {
-                alert('Settings saved!');
-            });
-        } else {
-            alert('Please fill in both Base URL and Redirect URL.');
+    chrome.storage.sync.get('rules', function (result) {
+        const existingRules = result.rules || [];
+        for (const rule of existingRules) {
+            addRuleToList(rule.baseURL, rule.redirectURL);
         }
     });
+
+    addRuleButton.addEventListener('click', function () {
+        const baseURL = baseURLInput.value.trim();
+        const redirectURL = redirectURLInput.value.trim();
+
+        if (baseURL && redirectURL) {
+            addRuleToList(baseURL, redirectURL);
+            baseURLInput.value = '';
+            redirectURLInput.value = '';
+
+            saveRulesToStorage();
+        }
+    });
+
+    rulesList.addEventListener('click', function (event) {
+        if (event.target.classList.contains('removeRule')) {
+            const ruleItem = event.target.parentElement;
+            removeRuleFromList(ruleItem);
+
+            saveRulesToStorage();
+        }
+    });
+
+    function addRuleToList(baseURL, redirectURL) {
+        const ruleItem = document.createElement('li');
+        ruleItem.setAttribute('data-baseurl', baseURL);
+        ruleItem.setAttribute('data-redirecturl', redirectURL);
+        ruleItem.textContent = baseURL + ' => ' + redirectURL;
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.classList.add('removeRule');
+        ruleItem.appendChild(removeButton);
+
+        rulesList.appendChild(ruleItem);
+    }
+
+    function removeRuleFromList(ruleItem) {
+        ruleItem.remove();
+    }
+
+    function saveRulesToStorage() {
+        const rules = [];
+        const ruleItems = rulesList.querySelectorAll('li');
+        ruleItems.forEach(function (ruleItem) {
+            const baseURL = ruleItem.getAttribute('data-baseurl');
+            const redirectURL = ruleItem.getAttribute('data-redirecturl');
+            rules.push({ baseURL, redirectURL });
+        });
+
+        chrome.storage.sync.set({ rules }, function () {
+            console.log('Redirecting rules saved!');
+        });
+    }
 });
